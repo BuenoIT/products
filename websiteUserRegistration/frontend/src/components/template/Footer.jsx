27f526cd6d8d913086a77d1aff77aa130aacb8b8
BuenoIT <101655079+BuenoIT@users.noreply.github.com>
2022-03-21 11:03:59 -0400

    commit on master

---
+++ websiteUserRegistration/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+      <span>
+        Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Gui
+        <span className="text-danger">Bueno</span>!</strong>
+      </span>
+    </footer>
